7ef0e36e163fca8418c3fedebe4037271c317e40
TFohrer <t.fohrer@gmx.de>
2020-04-20 17:22:31 +0200

    added new logo navigation scrolling effect for the home page

--- framework/views/global/_brand.php
+++ framework/views/global/_brand.php
@@ -9,12 +9,19 @@
 $site_name        = get_bloginfo( 'name' );
 $site_description = get_bloginfo( 'description' );
 $logo             = x_make_protocol_relative( x_get_option( 'x_logo' ) );
-$site_logo        = '<img src="' . $logo . '" alt="' . $site_description . '">';
+$logo_clean       = x_make_protocol_relative( get_stylesheet_directory_uri() . '/assets/images/logo_clean.png');
+$site_logo        = '<img class="nav-primary__logo" src="' . $logo . '" alt="' . $site_description . '">';
+$site_logo_clean  = '<img class="nav-primary__logo-clean" src="' . $logo_clean . '" alt="' . $site_description . '">';
 
 ?>
 
 <?php echo ( is_front_page() ) ? '<h1 class="visually-hidden">' . $site_name . '</h1>' : ''; ?>
 
 <a href="<?php echo home_url( '/' ); ?>" class="<?php x_brand_class(); ?>" title="<?php echo $site_description; ?>">
-  <?php echo ( $logo == '' ) ? $site_name : $site_logo; ?>
+  <?php
+        echo ( $logo == '' ) ? $site_name : $site_logo;
+        if($logo_clean !== '') {
+            echo $site_logo_clean;
+        }
+    ?>
 </a>
