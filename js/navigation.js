e850c0450b79857c48d3a859efcc3fe3636a7a27
TFohrer <t.fohrer@gmx.de>
2020-04-22 15:37:25 +0200

    fixed navbar for all other pages (not home -> not overlaying)

--- js/navigation.js
+++ js/navigation.js
@@ -3,10 +3,16 @@ import Headroom from 'headroom.js';
 (function(){
 
     function initHeadroom(){
-        const header = document.querySelector("header");
-        console.log(header);
+        const header = document.querySelector(".masthead");
         // construct an instance of Headroom, passing the element
-        const headroom  = new Headroom(header);
+        const headroom  = new Headroom(header, {
+            onTop: function(){
+              document.body.style.paddingTop = 0;
+            },
+            onNotTop: function(){
+                document.body.style.paddingTop = `${header.offsetHeight}px`;
+            }
+        });
         // initialise
         headroom.init();
     }
