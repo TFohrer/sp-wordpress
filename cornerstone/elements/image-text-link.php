32fc6044e5aea78c7d1cd86c01bf589d7642960b
Tarek Fohrer <tarek.fohrer@sinnerschrader.com>
2022-01-30 18:29:25 +0100

    feat: update custom element to support latest x-theme and cornerstone version

--- cornerstone/elements/image-text-link.php
+++ cornerstone/elements/image-text-link.php
@@ -1,71 +1,71 @@
 <?php
 
 cs_register_element('image-text-link', [
-    'title' => __('Image Text Link', 'domain'),
-    'values' => [
-        'headline' => cs_value('Headline goes here', 'markup', false),
-        'content' => cs_value('Content', 'markup', false),
-        'background_image' => cs_value('', 'markup', false),
-        'link' => cs_value('', 'style', false),
-    ],
-    'builder' => 'image_text_link_builder_setup',
-    // Connect a function used to render this element
-    'render' => 'image_text_link_render',
+  'title' => __('Image Text Link', 'domain'),
+  'values' => [
+    'headline' => cs_value('Headline goes here', 'markup', false),
+    'content' => cs_value('Content', 'markup', false),
+    'background_image' => cs_value('', 'markup', false),
+    'link' => cs_value('', 'style', false),
+  ],
+  'builder' => 'image_text_link_builder_setup',
+  // Connect a function used to render this element
+  'render' => 'image_text_link_render',
 ]);
 
 function image_text_link_builder_setup()
 {
-    return [
-        // Define the control groups that will appear in the inspector navigation bar
-        'control_nav' => [
-            'image-text-link' => __('Image Text Link', 'your-text-domain'),
-            'image-text-link:setup' => __('Setup', 'your-text-domain'),
-        ],
-        // Define the controls that connect to our values.
-        'controls_std_content' => [
-            [
-                'key' => 'headline',
-                'type' => 'text',
-                'group' => 'image-text-link:setup',
-                'label' => __('Headline', 'your-text-domain'),
-            ],
-            [
-                'key' => 'content',
-                'type' => 'text',
-                'group' => 'image-text-link:setup',
-                'label' => __('Content', 'your-text-domain'),
-            ],
-            [
-                'key' => 'background_image',
-                'type' => 'image',
-                'group' => 'image-text-link:setup',
-                'label' => __('Background Image', 'your-text-domain'),
-            ],
-            [
-                'key' => 'link',
-                'type' => 'text',
-                'group' => 'image-text-link:setup',
-                'label' => __('Link', 'your-text-domain'),
-            ],
-        ],
-    ];
+  return cs_compose_controls([
+    // Define the control groups that will appear in the inspector navigation bar
+    'control_nav' => [
+      'image-text-link' => __('Image Text Link', 'your-text-domain'),
+      'image-text-link:setup' => __('Setup', 'your-text-domain'),
+    ],
+    // Define the controls that connect to our values.
+    'controls_std_content' => [
+      [
+        'key' => 'headline',
+        'type' => 'text',
+        'group' => 'image-text-link:setup',
+        'label' => __('Headline', 'your-text-domain'),
+      ],
+      [
+        'key' => 'content',
+        'type' => 'text',
+        'group' => 'image-text-link:setup',
+        'label' => __('Content', 'your-text-domain'),
+      ],
+      [
+        'key' => 'background_image',
+        'type' => 'image',
+        'group' => 'image-text-link:setup',
+        'label' => __('Background Image', 'your-text-domain'),
+      ],
+      [
+        'key' => 'link',
+        'type' => 'text',
+        'group' => 'image-text-link:setup',
+        'label' => __('Link', 'your-text-domain'),
+      ],
+    ],
+  ]);
 }
 
 function image_text_link_render($data)
 {
-    extract($data);
+  extract($data);
 
-    $id = $id ?? '';
-    $mod_id = $mod_id ?? '';
-    $class = $class ?? '';
+  $id = $id ?? '';
+  $mod_id = $mod_id ?? '';
+  $class = $class ?? '';
 
-    $atts = cs_atts([
-        'id' => $id,
-        'class' => cs_attr_class($mod_id, 'image-text-link', $class),
-    ]);
+  $atts = cs_atts([
+    'id' => $id,
+    'class' => [$mod_id, 'image-text-link', $class],
+  ]);
 
-    ob_start();
-    ?>
+  ob_start();
+  ?>
     <a <?php echo $atts; ?> href="<?php echo $link; ?>">
         <img class="image-text-link__image" src="<?php echo $background_image; ?>">
         <div class="image-text-link__overlay-container">
